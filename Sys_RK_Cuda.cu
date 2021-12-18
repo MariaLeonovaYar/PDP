af0e10f37c9e7137e65a84a1efeffe0243300ce8
Maria Leonova <leonova-99@list.ru>
2021-12-18 08:13:30 +0300

    Create Sys_RK_Cuda.cu

---
+++ Sys_RK_Cuda.cu
@@ -0,0 +1,65 @@
+﻿#include <cmath>
+#include <iostream>
+
+#include <cuda.h>
+#include <cuda_runtime_api.h>
+
+__global__ void calculateK(float* k1, float *k2, float *k3, float *k4, float xn, float yn, float h, int i) {
+
+    float a = rand() % 1000;
+    float b = rand() % 1000;
+
+    k1[i] = h * cos(a * xn) - b * yn;
+    k2[i] = h * (cos(a * (xn + h / 2.0)) - b * (yn + k1[i] * h / 2.0));
+    k3[i] = h * (cos(a * (xn + h / 2.0)) - b * (yn + k2[i] * h / 2.0));
+    k4[i] = h * (cos(a * (xn + h)) - b * (yn + k3[i] * h));
+}
+
+__global__ void calculateRez(float* yn, float* k1, float* k2, float* k3, float* k4, float h, int i) {
+        yn[i] = yn[i] + (k1[i] + 2 * k2[i] + 2 * k3[i] + k4[i]) / 6.0;
+}
+
+int main() {
+
+    srand(NULL);
+
+    float x0 = 0, h = 0.0002, y0 = 0.8;
+    float xn = x0;
+    int finish = 10000, m = 100;
+    
+    float *k1, *k2, *k3, *k4, *yn;
+    float *c_k1, *c_k2, *c_k3, *c_k4, *c_yn;
+
+    k1 = (float*)malloc(m * sizeof(float));
+    k2 = (float*)malloc(m * sizeof(float));
+    k3 = (float*)malloc(m * sizeof(float));
+    k4 = (float*)malloc(m * sizeof(float));
+    yn = (float*)malloc(m * sizeof(float));
+
+    for (int i = 0; i < finish; i++) {
+
+        cudaMalloc((void**)&c_k1, m * sizeof(float));
+        cudaMalloc((void**)&c_k2, m * sizeof(float));
+        cudaMalloc((void**)&c_k3, m * sizeof(float));
+        cudaMalloc((void**)&c_k4, m * sizeof(float));
+        cudaMalloc((void**)&c_yn, m * sizeof(float));
+
+        calculateK << <1, 5 >> > (d_k1, d_k2, d_k3, d_k4, xn, y0, h, i);
+        calculateRez << <1, 5 >> > (d_yn, d_k1, d_k2, d_k3, d_k4, h, i);
+
+        cudaMemcpy(c_k1, k1, m * sizeof(float), cudaMemcpyDeviceToHost);
+        cudaMemcpy(c_k2, k2, m * sizeof(float), cudaMemcpyDeviceToHost);
+        cudaMemcpy(c_k3, k3, m * sizeof(float), cudaMemcpyDeviceToHost);
+        cudaMemcpy(c_k4, k4, m * sizeof(float), cudaMemcpyDeviceToHost);
+        cudaMemcpy(c_yn, yn, m * sizeof(float), cudaMemcpyDeviceToHost);
+
+        xn += h;
+    }
+
+    free(k1);
+    free(k2);
+    free(k3);
+    free(k4);
+    free(yn);
+    return 0;
+}
